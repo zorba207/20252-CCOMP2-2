c0f968f8c2e7577ffd330226177436e9dac5f2d1
Zorba <zorbacl2007@gmail.com>
2025-10-21 20:52:30 -0500

    codeforce 21/10/2025

---
+++ codeforce/Codeforces Round 1050 (Div. 4)/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //ejercicio A
+    /**int t;
+    cin >> t;
+    for (int i=0; i<t; i++){
+        int x,n;
+        cin >> x >> n;
+        int suma =0;
+        if (n%2==0){
+            suma = (x*(n/2))+((-1*x)*n/2);
+        }
+        else{
+            suma = (x*((n/2)+1))+((-1*x)*(n/2));
+        }
+
+        cout << suma << endl;
+    } */
+
+    //ejercicio B
+     /**int t;
+     cin >> t;
+     for (int i=0; i<t; i++){
+        int n,m,x,y;
+        cin >>n>>m>>x>>y;
+        int a[n];
+        for (int i=0; i<n; i++){
+            cin >> a[i];
+        }
+        int b[m];
+        for (int i=0; i<m; i++){
+            cin >> b[i];
+        }
+        int minimo=n+m;
+        cout << minimo << endl;
+
+     }*/
+
+    //ejercicio C
+    int t;
+    cin >> t;
+    for (int i=0; i<t; i++){
+        int n,m;
+        cin >> n,m;
+    }
+    int b[2]={0,1};
+
+
+    return 0;
+}
